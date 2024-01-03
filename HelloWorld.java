e6875bea8eaf622e2e3458c2c325b4f178a3e1f7
guzzzzman <155527944+guzzzzman@users.noreply.github.com>
2024-01-03 00:46:16 -0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,23 @@
+// Any line that starts with // is a Comment
+// Comments are "explanations", not considered as code by Java
+
+// On top of every program you need the following comments:
+// Name: Jose Guzman
+// Date: 8/27/2023
+// Description: Show "Hello World" in the command line / terminal
+// File Name: HelloWorld.java
+// To Compile in terminal type: javac HelloWorld.java
+// To Run the program in terminal type: java HelloWorld
+
+class HelloWorld
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}// end of main() method
+}//end of class HelloWorld
+
+/* Multiline comment that shows how program ran:
+ Hello World
+
+*/ 
